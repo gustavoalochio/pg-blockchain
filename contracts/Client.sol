7eb82ebab6cc52dc770b659a8a6152320c468ed9
Gustavo Alochio <gustavo.alochio@gmail.com>
2022-02-10 20:03:54 -0300

    Added contracts folder and removed md files

---
+++ contracts/Client.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
+
+contract ATestnetConsumer is ChainlinkClient, ConfirmedOwner {
+  using Chainlink for Chainlink.Request;
+
+  uint256 constant private ORACLE_PAYMENT = 1 * LINK_DIVISIBILITY;
+  bytes32 public message;
+
+  event RequestEthereumPriceFulfilled(
+    bytes32 indexed requestId,
+    bytes32 indexed message
+  );
+
+  constructor() ConfirmedOwner(msg.sender){
+    setPublicChainlinkToken();
+  }
+
+  function requestEthereumPrice(address _oracle, string memory _jobId)
+    public
+    onlyOwner
+  {
+    Chainlink.Request memory req = buildChainlinkRequest(stringToBytes32(_jobId), address(this), this.fulfill.selector);
+    req.add("id", "{\"id\": 0,\"data\":{\"id\": \"00:0D:B9:2F:56:72\"}}");
+    req.addInt("times", 100);
+    sendChainlinkRequestTo(_oracle, req, ORACLE_PAYMENT);
+  }
+
+  function fulfill(bytes32 _requestId, bytes32 _message)
+    public
+    recordChainlinkFulfillment(_requestId)
+  {
+    emit RequestEthereumPriceFulfilled(_requestId, _message);
+    message = _message;
+  }
+
+
+  function getChainlinkToken() public view returns (address) {
+    return chainlinkTokenAddress();
+  }
+
+  function withdrawLink() public onlyOwner {
+    LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
+    require(link.transfer(msg.sender, link.balanceOf(address(this))), "Unable to transfer");
+  }
+
+  function cancelRequest(
+    bytes32 _requestId,
+    uint256 _payment,
+    bytes4 _callbackFunctionId,
+    uint256 _expiration
+  )
+    public
+    onlyOwner
+  {
+    cancelChainlinkRequest(_requestId, _payment, _callbackFunctionId, _expiration);
+  }
+
+  function stringToBytes32(string memory source) private pure returns (bytes32 result) {
+    bytes memory tempEmptyStringTest = bytes(source);
+    if (tempEmptyStringTest.length == 0) {
+      return 0x0;
+    }
+
+    assembly { // solhint-disable-line no-inline-assembly
+      result := mload(add(source, 32))
+    }
+  }
+}
