bff5b0caa5db2f8f69b0fe76632bada8f67dbdd2
Gustavo Alochio <gustavo.alochio@gmail.com>
2022-03-05 21:53:50 -0300

    Updated validator

--- adapter/validator.py
+++ adapter/validator.py
@@ -2,13 +2,16 @@ from bridge import Bridge
 import ipfshttpclient
 import json
 import sys
+import requests
 
 class Validator:
-    # base_url = 'http://root:root@runtime:8888'
+#{'hashIpfs': 'IlFtZGFrQVNUb2RFcGdERTNrOGJDeVI4RTI2M3QxZW1zNEpKVWFlN1RCZHUyS1ciCg==', 'path': '/api/v1/wtps/00:0D:B9:2F:56:63'}
+
+    base_url = 'http://root:root@runtime:8888'
+    ipfs_url = 'http://ipfs:8080/'
 
     def __init__(self, input):
         self.request_data = input.get('data')
-        print(self.request_data, file=sys.stderr)
 
         if self.validate_request_data():
             self.bridge = Bridge()
@@ -23,18 +26,39 @@ class Validator:
             return False
         return True
 
+    def result_success(self, path):
+        print("SUCCESS: Rota: " + path + " verificada!", file=sys.stderr)
+        self.result = "SUCCESS: Rota: " + path + " verificada!"
+
+    def result_error(self, path):
+        print("ERROR: Rota: " + path + " corrompida!", file=sys.stderr)
+        self.result = "ERROR: Rota: " + path + " corrompida!"
+
     def logValidation(self):
         try:
             print(self.request_data, file=sys.stderr)
-            self.result_success(self.request_data)
+
+            #GET IPFS JSON
+            hashIpfs = (self.request_data['hashIpfs']).decode("utf-8")
+
+            if not hashIpfs or not hashIpfs.startswith('Qm'):
+                self.result_error("")
+
+            url = self.ipfs_url + hashIpfs
+            jsonIpfs = self.bridge.request(url)
+            print(jsonIpfs, file=sys.stderr)
+
+            #GET API JSON
+            url = self.base_url + self.request_data['path']
+            jsonApi = self.bridge.request(url)
+            print(jsonApi, file=sys.stderr)
+
+            if(jsonIpfs == jsonApi):
+                self.result_success(self.request_data['path'])
+            else:
+                self.result_success(self.request_data['path'])
+
         except Exception as e:
             self.result_error(e)
         finally:
             self.bridge.close()
-
-    def result_success(self, data):
-        self.result = "SUCESSO"
-
-    def result_error(self, error):
-        self.result = "ERROR"
-
