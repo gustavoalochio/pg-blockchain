8ed83bae69efbc54483da0f064868b165730872c
Gustavo Alochio <gustavo.alochio@gmail.com>
2022-03-05 22:13:12 -0300

    Updated validator

--- adapter/validator.py
+++ adapter/validator.py
@@ -8,7 +8,7 @@ class Validator:
 #{'hashIpfs': 'IlFtZGFrQVNUb2RFcGdERTNrOGJDeVI4RTI2M3QxZW1zNEpKVWFlN1RCZHUyS1ciCg==', 'path': '/api/v1/wtps/00:0D:B9:2F:56:63'}
 
     base_url = 'http://root:root@runtime:8888'
-    ipfs_url = 'http://ipfs:8080/'
+    ipfs_url = 'http://ipfs:8080/ipfs/'
 
     def __init__(self, input):
         self.request_data = input.get('data')
@@ -40,17 +40,17 @@ class Validator:
 
             #GET IPFS JSON
             hashIpfs = self.request_data['hashIpfs']
-            
+
             if not hashIpfs or not hashIpfs.startswith('Qm'):
                 self.result_error("")
 
             url = self.ipfs_url + hashIpfs
-            jsonIpfs = self.bridge.request(url)
+            jsonIpfs = self.bridge.request(url).json()
             print(jsonIpfs, file=sys.stderr)
 
             #GET API JSON
             url = self.base_url + self.request_data['path']
-            jsonApi = self.bridge.request(url)
+            jsonApi = self.bridge.request(url).json()
             print(jsonApi, file=sys.stderr)
 
             if(jsonIpfs == jsonApi):
