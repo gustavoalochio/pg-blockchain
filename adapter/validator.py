8eb3f78d7f8aa53395cc1773d9c8eb78c59cff15
Gustavo Alochio <gustavo.alochio@gmail.com>
2022-03-05 21:56:34 -0300

    Updated validator

--- adapter/validator.py
+++ adapter/validator.py
@@ -39,7 +39,7 @@ class Validator:
             print(self.request_data, file=sys.stderr)
 
             #GET IPFS JSON
-            hashIpfs = (self.request_data['hashIpfs']).decode("utf-8")
+            hashIpfs = bytes(self.request_data['hashIpfs']).decode("utf-8")
 
             if not hashIpfs or not hashIpfs.startswith('Qm'):
                 self.result_error("")
